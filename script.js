202c77121be06e8914c0d679f4b7b40179aea8ac
Jason Lam <nyjasonlam@gmail.com>
2021-05-16 21:31:18 -0400

    Added createNewEntry()"

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const { starterPrompt, addDepartmentPrompt } = require("./public/utils/inquirer_prompts");
+const {
+  starterPrompt,
+  addDepartmentPrompt,
+} = require("./public/utils/inquirer_prompts");
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "asdfjason",
-//   database: "trackday",
-// });
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "asdfjason",
+  database: "trackday",
+});
 
-// const afterConnection = () => {
-//   connection.query(
-//     "SELECT * FROM employee LEFT JOIN role on employee.role_id = role.role_id;",
-//     (err, res) => {
-//       if (err) throw err;
-//       console.log(res);
-//       connection.end();
-//     }
-//   );
-// };
-
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log(`connected as id ${connection.threadId}`);
-// });
-
-inquirer
-  .prompt(starterPrompt)
-  .then(({ mainOptions }) => {
-    switch(mainOptions) {
-      case "Add Department":
-        inquirer.prompt()
+const afterConnection = () => {
+  connection.query(
+    "SELECT * FROM employee LEFT JOIN role on employee.role_id = role.role_id;",
+    (err, res) => {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
     }
-  });
+  );
+};
+
+const createNewEntry = (table, values) => {
+  connection.query(
+    "INSERT INTO ? (name) VALUES (?)",
+    [table, values],
+    (err, res) => {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    }
+  );
+};
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+inquirer.prompt(starterPrompt).then(({ mainOptions }) => {
+  switch (mainOptions) {
+    case "Add Department":
+      let table = mysql.raw('department');
+      inquirer.prompt(addDepartmentPrompt).then(({ departmentName }) => {
+        createNewEntry(table, departmentName);
+      });
+  }
+});
